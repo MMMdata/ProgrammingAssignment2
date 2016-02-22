2c91cfa6aca90a144eba2d4de6d161962134ee2c
MMMdata <eric.erlebacher@gmail.com>
2016-02-21 23:04:02 -0800

    Added files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## There are two functions in this file: both are used to store and manipulate matrices 
 
-## Write a short comment describing this function
+## This function is designed to store a matrix in the cache to be used for inversion in the cachemean function below
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	inv <- NULL
+      temp <- function(y) {
+      	x <<- y
+      	inv <<- NULL
+      }
+      pull <- function() x
+      invertMatrix <- function(inverse) inv <<- inverse
+      pullInverse <- function() inv
+      list(temp = temp,
+      	pull = pull,
+      	invertMatrix = invertMatrix,
+      	pullInverse = pullInverse
+      	)
 }
 
 
-## Write a short comment describing this function
+## This function inverts the matrix created in the makeCacheMatrix function from above. If it's already been inverted, it just pulls it from the cache instead.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      ## Return a matrix that is the inverse of 'x'
+      inv <- x$pullInverse()
+      if(!is.null(inv)) {
+      	message("getting cached data")
+      	return(inv)
+      }
+      mtrx <- x$pull()
+      inv <- solve(mtrx, ...)
+      x$invertMatrix(inv)
+      return(inv)
 }
